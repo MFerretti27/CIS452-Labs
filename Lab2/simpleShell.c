7c3bc085ba1e2e910fb16c03bf692cd8f149b6c1
Matt Ferretti <Ferretti7@yahoo.com>
2023-01-28 15:24:20 -0500

    changed file structure

--- Lab2/simpleShell.c
+++ Lab2/simpleShell.c
@@ -20,21 +20,21 @@ int main(int argc, char *argv[]) {
         userInput[strlen(userInput) - 1] = '\0'; // Removes newline
    
         if ((strcmp(userInput, "Quit") == 0) || (strcmp(userInput, "quit") == 0)) {
-            printf("User Entered Quit\n\n");
+            printf("\nUser Entered Quit\n\n");
             exit(0);
         }
 
         // Tokenize the line using by using strtok(), splits a string into multiple based on separators
         char *word_1 = strtok(userInput, " ");
         char *word_2 = strtok(NULL, " ");  //  Continues tokenizing users input
-
+        
         //printf("\nWord1: [%s]\n", word_1); // Used for debugging, seeing how its tokenized
         //printf("Word2: [%s]\n\n", word_2);
 
         // Spawn a child process to execute the command
         forkResult = fork();
         if (forkResult == 0) {
-            printf("I am child with PID [%ld] Obeying your command\n\n", (long)getpid());
+            printf("\nI am child with PID [%ld] Obeying your Command\n\n", (long)getpid());
 
             char *argument_list[] = {word_1, word_2, NULL}; // NULL terminated array of char* strings
 
@@ -49,10 +49,9 @@ int main(int argc, char *argv[]) {
             printf("\nI am Parent with PID [%ld] Tracking Child's Statistics\n", (long)getpid());
 
             getrusage(RUSAGE_CHILDREN, &usage);
-            printf("\tChild's CPU time: %ld.%06d sec user, %ld.%06d sec system\n", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec,
-               usage.ru_stime.tv_sec, usage.ru_stime.tv_usec);
+            printf("\tChild's CPU time: %ld.%06d seconds\n", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
 
-            printf("\tNumber of involuntary context switches experienced by each individual child process: %ld\n", usage.ru_nivcsw);
+            printf("\tNumber of involuntary context switches experienced by the child process: %ld\n", usage.ru_nivcsw);
         }
 
         // Repeat until the user enters “quit"
